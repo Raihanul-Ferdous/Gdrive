c593565e084b9dc23bee5c2c59393f088bff507d
Raihanul-Ferdous <raihanul_rezve@yahoo.com>
2019-05-22 15:13:23 +0600

    Tried to upload and check file to gdrive - used ruby

---
+++ gdrive.rb
@@ -0,0 +1,13 @@
+file_metadata = {}
+file = drive_service.update_file(id,
+                                 file_metadata,
+                                 fields: 'id',
+                                 upload_source: 'files/photo.jpg',
+                                 content_type: 'image/jpeg')
+puts "File Id: #{file.id}"
+
+clint id
+
+902607644279-eum9khth3qp566t7jq0elie53n2g51br.apps.googleusercontent.com
+client secret
+byVHzb5G0fMShxsfYFM5O4e3
